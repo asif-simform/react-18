e8e634a20e7ee672a9096677c243178df7628722
asif-simform <asif.v@simformsolutions.com>
2022-07-21 12:57:53 +0530

    changes icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const onCallAPI = () => {
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "first_name": "hardik",
+  "last_name": "shakya",
+  "email": "JDsasa8008@mail.com",
+  "password": "Qwe@1234",
+  "phone_number": "+1 856-856-5897",
+  "profile_picture": "asdsfdffrfs",
+  "user_type": "Students",
+  "purpose_of_use": "created first user",
+  "device_token": "sdashdajsd233"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  // redirect: 'follow'
+};
+
+fetch("http://876b-14-99-102-226.ngrok.io/api/v1/user/signup", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +46,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={onCallAPI}>Call API For check</button>
       </header>
     </div>
   );
