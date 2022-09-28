1178a8a8fddc9c2fa7d438b54d2d2c190e8ffa7b
asif-simform <asif.v@simformsolutions.com>
2022-09-28 11:45:21 +0530

    fix : reduce bundle size

--- craco.config.js
+++ craco.config.js
@@ -1,24 +1,37 @@
 // const webpack = require('webpack');
 const CompressionPlugin = require("compression-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   //   reactScriptsVersion: "react-scripts" /* (default value) */,
 
-    externals: {
-      react: "React",
-      "react-dom": "ReactDOM",
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.externals = {
+        react: {
+          root: "React",
+          commonjs2: "react",
+          commonjs: "react",
+          amd: "react",
+        },
+        "react-dom": {
+          root: "ReactDOM",
+          commonjs2: "react-dom",
+          commonjs: "react-dom",
+          amd: "react-dom",
+        },
+      };
+      return webpackConfig;
     },
-    // plugins: [
-    //   new CompressionPlugin({
-    //     filename: "[path][base].gz",
-    //     algorithm: "gzip",
-    //     test: /\.js$|\.css$|\.html$/,
-    //     threshold: 10240,
-    //     minRatio: 0.8,
-    //   }),
-    // ],
+    plugins: [
+      new CompressionPlugin({
+        filename: "[path][base].gz",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ],
     optimization: {
       concatenateModules: true,
       flagIncludedChunks: true,
@@ -27,8 +40,7 @@ module.exports = {
         minSize: 1000 * 600,
       },
       minimize: true,
-      minimizer: [
-        new TerserPlugin(),
-      ],
+      minimizer: [new TerserPlugin()],
     },
+  },
 };
