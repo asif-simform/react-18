385bb9f33135993a70c630fc3fb50f3e1ea8cb03
asif-simform <asif.v@simformsolutions.com>
2022-10-04 18:24:06 +0530

    feat : lazy load components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { lazy, Suspense } from "react";
 import { AppContext } from "./context/Provider";
 // import Actions from "./components/Actions";
 // import Counter from "./components/Counter";
 // import Toast from "./components/Toast";
-import ToastContainer from "./containers/ToastContainer";
 // import TodoList from "./containers/TodoList";
-import Greed from "./containers/Greed";
 // import logo from "./logo.svg";
 import isMobileDeviceDetect from "./utils/deviceDetect";
 
 import "./App.css";
+
+const Greed = lazy(() =>
+  import(/* webpackChunkName: "Greed" */ "./containers/Greed")
+);
+
+const ToastContainer = lazy(() =>
+  import(/* webpackChunkName: "ToastContainer" */ "./containers/ToastContainer")
+);
 
 function App() {
   const isMobile = isMobileDeviceDetect();
@@ -19,7 +26,7 @@ function App() {
       <AppContext>
         <div className="App">
           <header className="App-header">
-            Hey, 
+            Hey,
             <h1>{JSON.stringify({ isMobile })}</h1>
             {/* <Toast /> */}
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
@@ -27,9 +34,13 @@ function App() {
             <Actions /> */}
             {/* <TodoList /> */}
           </header>
-          <Greed />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Greed />
+          </Suspense>
         </div>
-        <ToastContainer />
+        <Suspense>
+          <ToastContainer />
+        </Suspense>
       </AppContext>
     </>
   );
