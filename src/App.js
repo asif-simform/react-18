4787df81d60bcb0c0a693bf98db21d238f910ea6
asif-simform <asif.v@simformsolutions.com>
2024-02-02 12:19:16 +0530

    feat: Use setInterval using workers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import { lazy, Suspense, useState } from "react";
+import {
+  lazy,
+  Suspense,
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
 import AnimatedNumbers from "react-animated-numbers";
+import { clearInterval, setInterval } from "worker-timers";
 
 import { AppContext } from "./context/Provider";
 // import Actions from "./components/Actions";
 // import Counter from "./components/Counter";
 // import Toast from "./components/Toast";
-import NewApp from "./containers/NewApp";
-import ItemsList from "./containers/ItemsList";
+// import NewApp from "./containers/NewApp";
+// import ItemsList from "./containers/ItemsList";
 // import TodoList from "./containers/TodoList";
-import RoutingNumber from "./components/RoutingNumber/index";
+// import RoutingNumber from "./components/RoutingNumber/index";
 // import isMobileDeviceDetect from "./utils/deviceDetect";
 // import { fullDate } from "./utils/dateFormat";
-import { sum } from "app-18-demo/src/components/Button/index";
+// import { sum } from "app-18-demo/src/components/Button/index";
 import RouteApp from "./RouteApp";
 import "./App.css";
+import axios from "axios";
 
 // const Greed = lazy(() =>
 //   import(/* webpackChunkName: "Greed" */ "./containers/Greed")
@@ -25,9 +34,44 @@ const ToastContainer = lazy(() =>
 
 function App() {
   const [num, setNum] = useState(3539.55);
+  const [, setAppVisibility] = useState(false);
+  const [count, setCount] = useState(0);
+  const interval = useRef(null);
+
+  const clearIntervalTimer = useCallback(() => {
+    console.log(`0007 clearIntervalTimer Called`);
+
+    if (interval.current) {
+      console.log(`0007 clearIntervalTimer Called inside if con`);
+      try {
+        // NOTE: this is not window.clearTimeout, this is from the worker-timers package
+        // worker-timers version can throw if the id is invalid
+        clearInterval(interval.current);
+        interval.current = null;
+      } catch (e) {
+        // ignore
+        console.log(`clearInterval error :: ${e}`);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    const callback = () => {
+      setCount((count) => {
+        console.log(`Count is :: ${count + 1}`);
+        return count + 1;
+      });
+    };
+
+    interval.current = setInterval(callback, 1000);
+
+    return () => {
+      clearIntervalTimer();
+    };
+  }, []);
 
   // const isMobile = isMobileDeviceDetect();
-  sum(100, 200);
+  // sum(100, 200);
 
   const export2csv = async () => {
     // let data = "";
@@ -65,10 +109,42 @@ function App() {
   //   return <ItemsList />
   // }
 
+  const callAPI = () => {
+    const url = "http://localhost:8888/api.harboursquare/V1/rider/route_runs";
+
+    axios
+      .get(url)
+      .then(function (response) {
+        // handle success
+        console.log("response", response?.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error?.response?.data);
+      });
+  };
+
+  const onVisibilityChange = useCallback(() => {
+    if (document.hidden) {
+      setAppVisibility(true);
+    } else {
+      setAppVisibility(false);
+    }
+    console.log(`App Visibility Changed :: ${document.hidden}`);
+  }, [setAppVisibility]);
+
+  useEffect(() => {
+    document.addEventListener("visibilitychange", onVisibilityChange);
+    return () => {
+      document.removeEventListener("visibilitychange", onVisibilityChange);
+    };
+  }, [onVisibilityChange]);
+
   return (
     <AppContext>
       <button onClick={export2csv}>Export array to csv file</button>
-
+      <br />
+      <br />
       <a
         download="SupportUs_user_import.csv"
         // target='_blank'
@@ -76,29 +152,36 @@ function App() {
         rel="noreferrer"
         type="text/csv"
       >
-        download CSV template
+        Download CSV template
       </a>
       <br />
+      <br />
+      <br />
       <RouteApp />
-      <div className="container">
-      <AnimatedNumbers
-        includeComma
-        animateToNumber={num}
-        fontStyle={{ fontSize: 40 }}
-        configs={[
-          { mass: 1, tension: 220, friction: 100 },
-          { mass: 1, tension: 180, friction: 130 },
-          { mass: 1, tension: 280, friction: 90 },
-          { mass: 1, tension: 180, friction: 135 },
-          { mass: 1, tension: 260, friction: 100 },
-          { mass: 1, tension: 210, friction: 180 },
-        ]}
-      ></AnimatedNumbers>
-      <div>
-        <button onClick={() => setNum((state) => state + 31234)}>+</button>
-        <button onClick={() => setNum((state) => state - 31234)}>-</button>
+      <div
+        className="container"
+        style={{
+          display: "none",
+        }}
+      >
+        <AnimatedNumbers
+          includeComma
+          animateToNumber={num}
+          fontStyle={{ fontSize: 40 }}
+          configs={[
+            { mass: 1, tension: 220, friction: 100 },
+            { mass: 1, tension: 180, friction: 130 },
+            { mass: 1, tension: 280, friction: 90 },
+            { mass: 1, tension: 180, friction: 135 },
+            { mass: 1, tension: 260, friction: 100 },
+            { mass: 1, tension: 210, friction: 180 },
+          ]}
+        ></AnimatedNumbers>
+        <div>
+          <button onClick={() => setNum((state) => state + 31234)}>+</button>
+          <button onClick={() => setNum((state) => state - 31234)}>-</button>
+        </div>
       </div>
-    </div>
       <div>
         {/* <h1>{JSON.stringify({ isMobile })}</h1>
           <h1>{JSON.stringify({ fullDate })}</h1>
@@ -114,10 +197,23 @@ function App() {
       <Suspense>
         <ToastContainer />
       </Suspense>
-      <p>Commit-2</p>
-      <p>Commit-3</p>
-      <p>Commit-4</p>
-      <p>finale</p>
+
+      <button onClick={callAPI}>Call API</button>
+
+      <hr />
+
+      <h1>Timer</h1>
+
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "60px",
+        }}
+      >
+        <h5>{count}</h5>
+
+        <button onClick={clearIntervalTimer}>clearIntervalTimer</button>
+      </div>
     </AppContext>
   );
 }
